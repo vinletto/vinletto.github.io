8d89d2b4f831c9412bc0735f5e7cae21a8a73cb3
vinícius barletto <vinicius.barletto@gmail.com>
2021-12-18 10:00:50 -0300

    Update age-check.php

--- age-check.php
+++ age-check.php
@@ -14,15 +14,11 @@ echo "- Oi, eu tenho $idade." . PHP_EOL . PHP_EOL . PHP_EOL;
     } else {
         if ($idade >= 16 && $NumeroDePessoas > 1) {
             echo "- Vocé é menor de idade mas está acompanhado, pode entrar." . PHP_EOL . PHP_EOL;
-        } else {
-            if ($idade >= 16 && $NumeroDePessoas <= 1) {
+        } else if ($idade >= 16 && $NumeroDePessoas <= 1) {
                 echo "Você não pode entrar sozinho, com apenas $idade." . PHP_EOL . PHP_EOL;
-            } else {
-                if ($idade < 16 && $NumeroDePessoas > 1) {
+            } else if ($idade < 16 && $NumeroDePessoas > 1) {
                     echo "Você não pode entrar com apenas $idade, mesmo acompanhado.";
-                } else {
+        } else {
                     echo "Você está sozinho e é menor de idade, então não poderá entrar. Desculpe!";
-                }
-            }
         }
     }
