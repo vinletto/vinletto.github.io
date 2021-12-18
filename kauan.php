2de4c7b27ace8bb20ff68fee6f6b188f75d5bc0a
vinícius barletto <vinicius.barletto@gmail.com>
2021-12-18 14:08:51 -0300

    Add files via upload

---
+++ kauan.php
@@ -0,0 +1,14 @@
+<?php
+
+//Usuário definirá X e Y
+
+$Dinheiro = X;
+$OportunidadeDeCrescimento = Y;
+
+if ($Dinheiro > $OportunidadeDeCrescimento) {
+    echo "Parabéns mano, espero que você consiga ficar na empresa que te oferecer um salario melhor.";
+} else {
+    echo "Parabéns cachorro, torcendo pra que você evolua muito com essa oportunidade!";
+}
+
+echo PHP_EOL . "bejo do vinletto";
